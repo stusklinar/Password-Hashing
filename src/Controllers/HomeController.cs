927d8ee59f20efaedc4ba083bbc302f2bd0c053b
Stu Sklinar <stuart.sklinar@cap-hpi.com>
2019-01-17 09:38:11 +0000

    Code sample for hashing passwords

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PasswordHashing.Models;
+
+namespace PasswordHashing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPasswordHasher<User> passwordHasher;
+
+        public HomeController(IPasswordHasher<User> passwordHasher)
+        {
+            this.passwordHasher = passwordHasher;
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string password, IFormCollection collection)
+        {
+            var user = new User();
+
+            ViewBag.Hash = passwordHasher.HashPassword(user, password);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Validate(string hash, string password)
+        {
+            var user = new User();
+
+            ViewBag.HashResult = passwordHasher.VerifyHashedPassword(user,hash, password);
+            return View("Index");
+        }
+
+    }
+}
---
+++ src/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordHashing.Models
+{
+    public class User
+    {
+
+        public User()
+        {
+        }
+
+        public string Email => "test@test.com";
+        public string Password { get; set; }
+    }
+
+}
